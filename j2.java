c22b7ca806828aa21c8e203121567dfc1dd4d471
2000031835_veerababu <2000031835@kluniversity.in>
2023-01-28 12:20:14 +0530

    welcome to azure

---
+++ j2.java
@@ -0,0 +1,9 @@
+public class SubtactOfNumbers
+{  
+public static void main(String args[])   
+{  
+int n1 = 225, n2 = 115, sub;  
+sub = n1 - n2;  
+System.out.println("The sum of numbers is: "+sub);  
+}  
+} 
